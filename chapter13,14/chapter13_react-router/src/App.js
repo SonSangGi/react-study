40ef9a7c9c99f6002cc1ae9e0392dc59ae1078da
sgson <ssg3799@gmail.com>
2020-02-01 22:49:52 +0900

    learning chapter13 [  sub router ]

--- chapter13,14/chapter13_react-router/src/App.js
+++ chapter13,14/chapter13_react-router/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
-import Profile from "./components/Profile";
+import Profiles from "./components/Profiles";
 
 const App = () => {
   return (
@@ -15,16 +15,13 @@ const App = () => {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/sanggi">sanggi 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/hyunho">hyunho 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 };
