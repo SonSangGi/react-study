365e2ddb3f2aa334cfc2086e2356b7e08622a354
sgson <ssg3799@gmail.com>
2020-02-02 02:09:10 +0900

    learning chapter13 [ Link, NavLink ]

--- chapter13,14/chapter13-react-router/src/App.js
+++ chapter13,14/chapter13-react-router/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Profiles from "./components/Profiles";
@@ -7,32 +7,59 @@ import HistorySample from "./components/HistorySample";
 import HistorySample2 from "./components/HistorySample2";
 import WithRouterSample from "./components/WithRouterSample";
 
+// NavLink 사용 시 default path인 "/"는 모든 Link에서 active상태임. 해결 방법은??
+
 const App = () => {
+  const activeStyle = {
+    background: "black",
+    color: "white"
+  };
   return (
     <div>
       <ul>
         <li>
-          <Link to="/">홈</Link>
+          <NavLink to="/" active>
+            홈
+          </NavLink>
         </li>
         <li>
-          <Link to="/about">소개</Link>
+          <NavLink activeStyle={activeStyle} to="/about">
+            소개
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles">프로필</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles">
+            프로필
+          </NavLink>
         </li>
         <li>
-          <Link to="/history">History 예제</Link>
+          <NavLink activeStyle={activeStyle} to="/history">
+            History 예제
+          </NavLink>
         </li>
         <li>
-          <Link to="/history2">History 예제2</Link>
+          <NavLink activeStyle={activeStyle} to="/history2">
+            History 예제2
+          </NavLink>
         </li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact={true} />
-      <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
-      <Route path="/history2" component={HistorySample2} />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={["/about", "/info"]} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route path="/history2" component={HistorySample2} />
+        <Route
+          //path를 따로 정의하지 않으면 모든 상황에 렌더링됨
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
--- chapter13,14/chapter13-react-router/src/components/Profiles.js
+++ chapter13,14/chapter13-react-router/src/components/Profiles.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import Profile from "./Profile";
 
+// NavLink : Link와 비슷, 현재 경로와 Link에서 사용하는 경로가 일치하는 경우 스타일 혹은 CSS 클래스를 적용할 수 있다.
+
 const Profiles = () => {
+  const activeStyle = {
+    background: "black",
+    color: "white"
+  };
   return (
     <div>
       <h3>사용자 목록:</h3>
       <ul>
         <li>
-          <Link to="/profiles/sanggi">sanggi</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/sanggi" active>
+            sanggi
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/hyunho">hyunho</Link>
+          <NavLink activeStyle={activeStyle} to="/profiles/hyunho">
+            hyunho
+          </NavLink>
         </li>
       </ul>
 
