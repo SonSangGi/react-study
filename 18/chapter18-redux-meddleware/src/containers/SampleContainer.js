d05a9ac69c6768aa6f0175590f59bf472e86463a
sgson <ssg3799@gmail.com>
2020-02-13 13:47:03 +0900

    Learn chapter18 [ redux-thunk-refactoring ]

--- 18/chapter18-redux-meddleware/src/containers/SampleContainer.js
+++ 18/chapter18-redux-meddleware/src/containers/SampleContainer.js
@@ -12,8 +12,17 @@ const SampleContainer = ({
   loadingUsers,
 }) => {
   useEffect(() => {
-    getPost(1);
-    getUsers(1);
+    // useEffect에 파라미터로 넣는 함수는 async로 할 수 없기 때문에
+    // 그 내부에서 async 함수를 선언하고 호출해준다.
+    const fn = async () => {
+      try {
+        getPost(1);
+        getUsers(1);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fn();
   }, [getPost, getUsers]);
   return (
     <Sample
@@ -26,11 +35,11 @@ const SampleContainer = ({
 };
 
 export default connect(
-  ({ sample }) => ({
+  ({ sample, loading }) => ({
     post: sample.post,
     users: sample.users,
-    loadingPost: sample.loading.GET_POST,
-    loadingUsers: sample.loading.GET_USERS,
+    loadingPost: loading.GET_POST,
+    loadingUsers: loading.GET_USERS,
   }),
   { getPost, getUsers },
 )(SampleContainer);
