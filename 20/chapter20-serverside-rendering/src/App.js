1142cb5e57d4f1ff98c6493a34db83fc7277024a
sgson <ssg3799@gmail.com>
2020-03-12 23:04:10 +0900

    Learn chapter20 [ serverside-rendering ]

---
+++ 20/chapter20-serverside-rendering/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import Menu from './components/Menu';
+import RedPage from './pages/RedPage';
+import BluePage from './pages/BluePage';
+import { Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <Menu />
+      <hr />
+      <Route path="/red" component={RedPage} />
+      <Route path="/blue" component={BluePage} />
+    </div>
+  );
+};
+
+export default App;
